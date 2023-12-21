939a2b51fe8f161cf3b1f55a627b9dbfb855b84e
stefanelli990 <stefan.mohenski@gmail.com>
2023-12-21 12:03:51 +0100

    removed animations

--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,6 +1,5 @@
 <template>
   <ul class="max-w-6xl mx-auto grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-    <TransitionGroup>
       <NoteItem
         v-for="note in storeNotes.filterNotes"
         :key="note.id"
@@ -10,7 +9,6 @@
         :date="note.date"
         :id="note.id"
       />
-    </TransitionGroup>
   </ul>
 </template>
 
@@ -20,34 +18,3 @@ import { useStoreNotes } from "../stores/storeNotes";
 
 const storeNotes = useStoreNotes();
 </script>
-
-<style scoped>
-.v-enter-from {
-  opacity: 0;
-  scale: .5;
-}
-
-.v-enter-to {
-  opacity: 1;
-  scale: 1;
-}
-
-.v-enter-active,
-.v-leave-active,
-.v-move {
-  transition: all 0.3s;
-}
-
-.v-leave-active {
-  position: absolute;
-}
-
-.v-leave-from {
-  opacity: 1;
-  scale: 1;
-}
-.v-leave-to {
-  opacity: 0;
-  scale: .5;
-}
-</style>
